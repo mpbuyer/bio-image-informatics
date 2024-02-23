fa255c95736e90f86911bfd77f9eb76973e3be45
mpbuyer <101243551+mpbuyer@users.noreply.github.com>
2024-02-23 09:19:38 -0700

    Update train_MIMIC.py

--- train_MIMIC.py
+++ train_MIMIC.py
@@ -30,48 +30,50 @@ def main(num_epochs, gpu_ids, batch_size, pretrained):
     if len(gpu_ids) > 0:
         os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu_ids[0])
 
-    # device = torch.device('cuda:{}'.format(gpu_ids[0])) if gpu_ids else torch.device('cpu')
-    device = torch.device("mps") 
+    device = torch.device('cuda:{}'.format(gpu_ids[0])) if gpu_ids else torch.device('cpu')
+    # device = torch.device("mps") 
 
     print('==> Preparing data...')
     # data transforms
     data_transforms = transforms.Compose(
-            [transforms.Resize((2048, 2048), interpolation=PIL.Image.NEAREST), 
+            [transforms.Resize((1024,1024), interpolation=transforms.InterpolationMode.BILINEAR), 
             transforms.ToTensor(),
             transforms.Normalize(mean=[.5], std=[.5])])
    
-    # current_dir = os.path.dirname(os.path.realpath(__file__))
     current_dir = "/scratch/mpbuyer/MIMIC-CXR-JPG-SAMPLE-1K"
-    # print(current_dir)
-    # print(os.getcwd())
-    
     # current_dir = "MIMIC-CXR-JPG-SAMPLE-1K"
+    # cpus = os.cpu_count()
+
     train_dataset = MimicDataset(current_dir, data_transforms, "train")
     val_dataset = MimicDataset(current_dir, data_transforms, "validate")
     test_dataset = MimicDataset(current_dir, data_transforms, "test")
 
-    
+    # request 7-8 cpus if there are 7 workers
     train_loader = data.DataLoader(dataset=train_dataset,
                                 batch_size=batch_size,
-                                shuffle=True)
+                                shuffle=True,
+                                num_workers = 7)
     val_loader = data.DataLoader(dataset=val_dataset,
                                 batch_size=batch_size,
-                                shuffle=False)
+                                shuffle=False,
+                                num_workers = 7)
     test_loader = data.DataLoader(dataset=test_dataset,
                                 batch_size=batch_size,
-                                shuffle=False)
+                                shuffle=False,
+                                num_workers = 7)
 
     print('==> Building and training model...')
     
     n_classes = 14*4 # 14 diseases, 4 categories
 
     if pretrained:
-        model = models.swin_v2_s(weights=models.Swin_V2_S_Weights.DEFAULT)
+        model = models.swin_v2_b(weights=models.Swin_V2_B_Weights.DEFAULT)
     else:
-        model = models.swin_v2_s()
+        model = models.swin_v2_b()
     num_features = model.head.in_features
-    model.fc = nn.Linear(num_features, n_classes)
+    model.head = nn.Linear(num_features, n_classes)
     model = model.to(device)
+    #print(model)
 
     # which loss?
     criterion = nn.BCEWithLogitsLoss()
@@ -85,11 +87,7 @@ def main(num_epochs, gpu_ids, batch_size, pretrained):
 
     best_auc = 0
     best_epoch = 0
-    best_model = deepcopy(model.to("cpu"))
-
-    global iteration
-    iteration = 0
-    best_auc = 0
+    best_model = deepcopy(model).to("cpu")
     
     for epoch in range(num_epochs):
         print("Epoch: ", epoch + 1, "-----------")        
@@ -104,11 +102,11 @@ def main(num_epochs, gpu_ids, batch_size, pretrained):
         print(f'Validation AUC: {val_auc:.4f}')
             
         if val_auc > best_auc:
-            best_epoch = epoch
+            best_epoch = epoch 
             best_auc = val_auc
-            best_model = deepcopy(model.to("cpu"))
+            best_model = deepcopy(model).to("cpu")
             print('cur_best_auc:', best_auc)
-            print('cur_best_epoch', best_epoch)
+            print('cur_best_epoch', best_epoch + 1)
         
         print("--------------------------------")
 
@@ -119,7 +117,10 @@ def main(num_epochs, gpu_ids, batch_size, pretrained):
     path = os.path.join(current_dir, 'best_model.pth')
     torch.save(state, path)
 
-    test_loss, test_acc, test_auc = test(best_model, val_loader, criterion, optimizer, device)
+    model = model.to("cpu")
+    best_model = best_model.to(device)
+
+    test_loss, test_acc, test_auc = test(best_model, test_loader, criterion, device)
     print(f'Test Loss: {test_loss:.4f}')
     print(f'Test ACC: {test_acc:.4f}')
     print(f'Test AUC: {test_auc:.4f}')
@@ -127,11 +128,11 @@ def main(num_epochs, gpu_ids, batch_size, pretrained):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='RUN Swin on MIMIC (or ChestXray-14)')
+        description='Train Swin on MIMIC')
 
     parser.add_argument('--num_epochs',
                         default=10,
-                        help='num of epochs of training, the script would only test model if set num_epochs to 0',
+                        help='number of epochs of training',
                         type=int)
     parser.add_argument('--gpu_ids',
                         default='0',
@@ -140,7 +141,7 @@ if __name__ == '__main__':
                         default=16,
                         type=int)
     parser.add_argument('--pretrained',
-                        help='Pretrain the model on ImageNet',
+                        help='Use ImageNet weights',
                         action="store_true")
 
 
